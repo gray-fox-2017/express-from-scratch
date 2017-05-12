583655d1d712c4366d5da2d60fa68ed78b0c476a
hakiemaul <noersedya@gmail.com>
2017-05-13 04:47:56 +0700

    express done

---
+++ seeders/20170512080612-seed-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      username: 'John Doe',
+      email: 'johndoe@mail.com'
+    }, {
+      username: 'Foo Bar',
+      email: 'foobar@mails.com'
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Person', null, {})
+  }
+};
