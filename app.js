ea927ebdefcf87334068e37dfd761c6942a90e90
raynormw <tirtawiryaputra@yahoo.com>
2017-05-15 01:25:31 +0700

    remove unused variable

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require('express');
 let app = express();
 let index = require('./routes/index');
 let user = require('./routes/user');
-let action = require('./routes/action');
 
 app.set('view engine', 'ejs');
 app.use('/', index);
 app.use('/', user);
-app.use('/', action);
 
 app.listen(3000, () => console.log('Listening on port 3000..'));
